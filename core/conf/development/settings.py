80330ffbb6e37a70f340bc2a9ec58077370672a3
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-11-18 16:41:02 -0300

    teste

--- core/conf/development/settings.py
+++ core/conf/development/settings.py
@@ -145,7 +145,7 @@ DATABASES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/code/staticfiles'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
