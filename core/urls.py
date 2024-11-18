874f23cbea2a634bed15fe6ffefa807b3e313695
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-11-18 14:58:23 -0300

    try

--- core/urls.py
+++ core/urls.py
@@ -6,12 +6,12 @@ Copyright (c) 2019 - present AppSeed.us
 from django import views
 from django.contrib import admin
 from django.urls import path, include  # add this
-from apps.registro import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),             
     path('accounts/', include('django.contrib.auth.urls')),  
-    path('register/', views.register, name='register'),
+  #  path('register/', views.register, name='register'),
     
    
     path('', include('apps.registro.urls')),  
