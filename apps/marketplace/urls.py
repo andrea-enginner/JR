61e7be6afa89f9d481bbe56912cea1ce8c84da72
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-11-24 01:56:29 -0300

    update: new crud to product

--- apps/marketplace/urls.py
+++ apps/marketplace/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # Certifique-se de importar as views corretamente
 
 urlpatterns = [
-    path('', views.marketplace_view, name='marketplace'),  # Defina a view para a rota principal
+    path('lanches/', views.marketplace_view, name='marketplace'),  # Defina a view para a rota principal
 ]
