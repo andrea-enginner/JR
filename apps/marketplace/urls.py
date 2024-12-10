7ff4d204f82279628656abee210e13866b602a96
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-12-10 06:25:01 -0300

    update: merged code

--- apps/marketplace/urls.py
+++ apps/marketplace/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # Certifique-se de importar as views corretamente
 
 urlpatterns = [
-    path('lanches/', views.marketplace_view, name='marketplace'),  # Defina a view para a rota principal
+    path('', views.marketplace_view, name='marketplace'),  # Defina a view para a rota principal
 ]
