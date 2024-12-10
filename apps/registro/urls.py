56674d19b00af1dde8443d679a63ce105294502f
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-12-10 06:32:48 -0300

    update: merged code

--- apps/registro/urls.py
+++ apps/registro/urls.py
@@ -9,7 +9,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
-from registro.models import Chat
+from apps.registro.models import Chat
 
 # Registrar os ViewSets
 router = DefaultRouter()
